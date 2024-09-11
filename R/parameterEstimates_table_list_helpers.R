b5fedff840ed8c2942fff83c67d3598fc2aa3295
Shu Fai Cheung <shufai.cheung@gmail.com>
2024-09-12 07:59:31 +0800

    Do not use sort_by

--- R/parameterEstimates_table_list_helpers.R
+++ R/parameterEstimates_table_list_helpers.R
@@ -754,13 +754,13 @@ insert_lhs <- function(est_i) {
         est_i$tmp0b <- match(est_i$efa, efa0)
         est_i$tmp1 <- seq_len(nrow(est_i))
         # R > 4.4.0
-        if (getRversion() > "4.4.0") {
-            est_i <- base::sort_by(est_i, ~ tmp0b + tmp0 + tmp1)
-          } else {
+        # if (getRversion() > "4.4.0") {
+        #     est_i <- base::sort_by(est_i, ~ tmp0b + tmp0 + tmp1)
+          # } else {
             est_i <- est_i[order(est_i$tmp0b,
                                  est_i$tmp0,
                                  est_i$tmp1), ]
-          }
+          # }
       } else {
         lhs0 <- unique(est_i$lhs)
         est_i$tmp0 <- match(est_i$lhs, lhs0)
